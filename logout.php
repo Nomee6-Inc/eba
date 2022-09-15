367fedf7e26c8241b80f82cd00102a21d8800f11
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-09-15 18:39:19 +0300

    Session Handler
    - Yeni Oturum mekanizmasına uyum sağlaması açısından yeni bir çıkış yapma sistemi yapıldı.

--- logout.php
+++ logout.php
@@ -1,8 +1,21 @@
-<?php 
-
+<?php
 session_start();
+include 'config.php';
+error_reporting(0);
 session_destroy();
 
-header("Location: index.php");
+$getsesscookie = $_COOKIE['sess_id'];
 
+$query = $db->prepare("UPDATE sessions SET
+logged_in = :new_logged_in
+WHERE sess_token = :sesstoken");
+$logoutquery = $query->execute(array(
+     "new_logged_in" => "0",
+     "sesstoken" => "$getsesscookie"
+));
+if ( $logoutquery ){
+}
+
+setcookie("sess_id", "", time() - 3600, "/");
+header("Refresh:1 url=index.php");
 ?>
